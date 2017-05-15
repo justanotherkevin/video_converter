a5790e6737547cbf95f68ca1ce3ea1be60b40621
kevin hu <hu.kevin.w@gmail.com>
2017-05-15 12:22:19 -0400

    Add sidekiq HardWorker class

--- app/workers/hard_worker.rb
+++ app/workers/hard_worker.rb
@@ -1,7 +1,16 @@
+
+
 class HardWorker
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  # def perform(*args)
+  #   # Do something
+  # end
+  def perform(name, count)
+  # do something
+    puts name
+    puts '###################'
+    puts count
   end
+
 end
