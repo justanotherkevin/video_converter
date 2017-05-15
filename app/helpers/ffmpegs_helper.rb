1d86915ba78153565aebaa6540adee212403c90d
kevin hu <hu.kevin.w@gmail.com>
2017-05-15 12:23:16 -0400

    Adding helper for ffmpeg

--- app/helpers/ffmpegs_helper.rb
+++ app/helpers/ffmpegs_helper.rb
@@ -1,2 +1,9 @@
 module FfmpegsHelper
+    def ffmpeg_and_sox(object)
+        ffmpeg_object = Ffmpeg.new(object)
+        aduio_rip = object[:audio]
+        new_file = AudioHero::Sox.new(aduio_rip.tempfile).convert({output_options: "-c 1 -b 16 -r 16k", output_format: "flac", channel:1})
+        @ffmpeg.audio = new_file
+
+    end
 end
