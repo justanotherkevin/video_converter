5904d1f8b0d72c7c5508014bf29ae9dba573e2b3
kevin hu <hu.kevin.w@gmail.com>
2017-05-14 20:57:53 -0400

    Add sidekiq worker ruby file

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,9 @@
-CarrierWave.configure do |config|
-  # config.fog_provider = 'fog/google'                        # required
-  config.fog_credentials = {
-    provider:                         'Google',
-    google_storage_access_key_id:     'GOOG2ZECDH3C22TQHUEE',
-    google_storage_secret_access_key: 'e6iphevn+2YJ5arTWovl0hd1Tx6vAvjoDjLyrWwz	'
-  }
-  config.fog_directory = 'videotospeechondemoforclient'
-end
+# CarrierWave.configure do |config|
+#   # config.fog_provider = 'fog/google'                        # required
+#   config.fog_credentials = {
+#     provider:                         'Google',
+#     google_storage_access_key_id:     'GOOG2ZECDH3C22TQHUEE',
+#     google_storage_secret_access_key: 'e6iphevn+2YJ5arTWovl0hd1Tx6vAvjoDjLyrWwz	'
+#   }
+#   config.fog_directory = 'videotospeechondemoforclient'
+# end
